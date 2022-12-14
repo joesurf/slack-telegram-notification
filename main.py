debd67aa45d47baf9f3f92d4c71de01736fed660
joesurf <desictiverk@gmail.com>
2022-12-14 23:09:10 +0800

    Add emojis

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def detect_messages(message, ack, say, client):
 
     msg = message["text"]
     content = f"""
-        You have a message from {sender} in Slack! {emoji.emojize(':mailbox_with_mail')}\
+        You have a message from {sender} in Slack! {emoji.emojize(':mailbox_with_mail:')}\
         \n\nhttps://slack.com/app_redirect?channel={channel}"""
 
 
