942fcf4b1cd0765cace90a1ce0250a42746e0185
joesurf <desictiverk@gmail.com>
2023-01-08 22:34:58 +0800

    Remove from deploy

--- main.py
+++ main.py
@@ -297,7 +297,7 @@ from slack_bolt.adapter.flask import SlackRequestHandler
 
 flask_app = Flask(__name__)
 handler = SlackRequestHandler(app)
-main()
+# main()
 
 @flask_app.route("/slack/events", methods=["POST"])
 def slack_events():
